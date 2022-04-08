4395f29325428539016a57c489902c58b0589b1d
Lionel <lionelgutierrez@gmail.com>
2022-04-08 18:16:52 -0300

    Se completan los ejercicios de la clase 2 y 3

--- Ejercicios-clase3/integrador.py
+++ Ejercicios-clase3/integrador.py
@@ -71,14 +71,8 @@ reg_nans_media = RegresionLineal()
 reg_nans_media.fit(X_train_nans_media_reducido_pca,trainY)
 
 # Paso 8 calcular MSE sobre validacion contra modelo sin nans y con media por nans y comparar
-# Escalo los datos y calculo los componentes de PCA para cada caso
-X_test_sin_nans = scaler.fit_transform(X_test_sin_nans)
-X_test_nans_media = scaler.fit_transform(X_test_nans_media)
-media_X_test_sin_nans  = np.nanmean(X_test_sin_nans,axis=0,keepdims=True).reshape(X_test_sin_nans.shape[1],1)
-media_X_test_nans_media  = np.nanmean(X_test_nans_media,axis=0,keepdims=True).reshape(X_test_nans_media.shape[1],1)
-
-X_test_sin_nans_reducido_pca = (X_test_sin_nans.dot( pca_sin_nans.components_.T))#+ (media_X_test_sin_nans[:num_dimensiones]).T
-X_test_nans_media_reducido_pca = (X_test_nans_media.dot( pca_nan_media.components_.T))#+ (media_X_test_nans_media[:num_dimensiones]).T
+X_test_sin_nans_reducido_pca = (X_test_sin_nans.dot( pca_sin_nans.components_.T))
+X_test_nans_media_reducido_pca = (X_test_nans_media.dot( pca_nan_media.components_.T))
 
 Y_reg_sin_nans = reg_sin_nans.predict(X_test_sin_nans_reducido_pca)
 Y_reg_nans_media = reg_nans_media.predict(X_test_nans_media_reducido_pca)
@@ -90,19 +84,6 @@ mse_nans_media = MSE(truth=testY,prediction=Y_reg_nans_media)
 print("MSE para el dataset con tratamiento de eliminar nans: ",mse_sin_nans())
 print("MSE para el dataset con media para nans: ",mse_nans_media())
 
-#Si no escalo los datos de test antes de reducir los componentes obtengo:
-#MSE para el dataset con tratamiento de eliminar nans:  32256.079276692035
-#MSE para el dataset con media para nans:  23657.686089238992
-
-# Si escalo los datos y calculo los componentes de PCA para cada caso
-#MSE para el dataset con tratamiento de eliminar nans:  153.37323514713935
-#MSE para el dataset con media para nans:  296.2013160636221
-
-# El MSE si escalo y comparo los datos de test y train escalados me dan que el metodo de eliminar nans es mejor que el metodo de la media
-# para este caso.
-# Si no escalara los datos de test el resultado es el opuesto
-
-#Si escalo los datos y los ajusto por su media el valor que obtengo es peor
-#MSE para el dataset con tratamiento de eliminar nans:  728.7883248687206
-#MSE para el dataset con media para nans:  531.474378518453
+#### CONCLUSIONES #####
+# El MSE para el dataset con los nans reemplazados por la media de la feature se comporta mejor que al eliminar los nans directamente
 
